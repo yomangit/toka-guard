ae1f361b966d95d4921a19c11ee39b5273309080
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-18 12:48:35 +0800

    dsdd

--- resources/views/components/input-field.blade.php
+++ resources/views/components/input-field.blade.php
@@ -1,22 +1,13 @@
-<div>
-    <fieldset class="fieldset">
+<fieldset class="fieldset">
     @if($label)
-        <x-form.label :label="$label" :required="$required" />
+    <x-form.label :label="$label" :required="$required" />
     @endif
 
-    <input
-        id="{{ $id }}"
-        name="{{ $name }}"
-        type="{{ $type }}"
-        {{ $attributes->merge([
+    <input id="{{ $id }}" name="{{ $name }}" type="{{ $type }}" {{ $attributes->merge([
             'class' =>
                 'input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs ' .
                 ($errors->has($name) ? 'ring-1 ring-rose-500 focus:ring-rose-500 focus:border-rose-500' : '')
-        ]) }}
-        placeholder="{{ $placeholder }}"
-        @if($wireModel) wire:model.live="{{ $wireModel }}" @endif
-    />
+        ]) }} placeholder="{{ $placeholder }}" @if($wireModel) wire:model.live="{{ $wireModel }}" @endif />
 
     <x-label-error :messages="$errors->get($name)" />
 </fieldset>
-</div>
