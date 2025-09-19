d81c8b78761bd518535d217ca155d1227009220a
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-20 06:58:10 +0800

    as

--- resources/views/livewire/hazard/hazard-detail.blade.php
+++ resources/views/livewire/hazard/hazard-detail.blade.php
@@ -576,7 +576,7 @@
                                     <div class="mb-2 md:mb-0">
                                         <p><strong>{!! $act['description'] !!}</strong></p>
                                         <p class="text-sm text-gray-500">
-                                            Batas Waktu Penyelesaian: {{ $act['due_date']  }}  |
+                                            Batas Waktu Penyelesaian: {{ \Carbon\Carbon::parse($act['due_date'])->timezone('Asia/Makassar')->format('d-m-Y') }}  |
                                             Tanggal Penyelesaian Tindakan: {{ $act['actual_close_date'] }} |
                                             PIC: {{ optional(\App\Models\User::find($act['responsible_id']))->name }}
                                         </p>
