8731febbea44a82625373f192859c82c71e5aaeb
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-19 21:14:58 +0800

    sdsd

--- resources/views/livewire/hazard/hazard-detail.blade.php
+++ resources/views/livewire/hazard/hazard-detail.blade.php
@@ -581,7 +581,7 @@
                                             PIC: {{ optional(\App\Models\User::find($act['responsible_id']))->name }}
                                         </p>
                                     </div>
-                                    <flux:button  wire:click="loadEditAction({{  $act['id'] }})"  size="xs" icon="pencil-square" variant="subtle"> Edit</flux:button>
+                                    <flux:button variant="subtle" size="sm" wire:click="loadEditAction({{ $act['id'] }})" icon="pencil-square"> Edit </flux:button>
                                     <flux:button wire:click="removeAction({{  $act['id'] }})" wire:confirm="Yakin hapus tindakan ini?" size="xs" icon="trash" variant="danger">Hapus</flux:button>
                                 </li>
                                 @empty
