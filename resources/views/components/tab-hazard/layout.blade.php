5f82584cd76029c8692a7b0851ddefdb57d202df
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-22 07:59:14 +0800

    fgfg

--- resources/views/components/tab-hazard/layout.blade.php
+++ resources/views/components/tab-hazard/layout.blade.php
@@ -1,7 +1,7 @@
 <div class="flex items-start max-md:flex-col ">
     <div class="flex-1 self-stretch max-md:pt-4">
         <div class="flex w-full flex-1 flex-col gap-4 rounded-xl 
-            max-h-[calc(100vh-14rem)] 
+            max-h-[calc(100vh-20rem)] 
             sm:max-h-[calc(100vh-9rem)] 
             md:max-h-[calc(100vh-14rem)] 
             lg:max-h-[calc(100vh-16rem)] 
