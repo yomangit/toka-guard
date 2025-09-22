f264ceee4842f0c77665d9932ede3aed31f81207
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-22 13:36:11 +0800

    sas

--- resources/views/partials/breadcrumbs.blade.php
+++ resources/views/partials/breadcrumbs.blade.php
@@ -8,21 +8,23 @@ $crumbs = ($routeName && $routeName !== 'livewire.update' && Breadcrumbs::exists
 @endphp
 
 @if ($crumbs->isNotEmpty())
-<nav class="breadcrumbs text-xs font-semibold hidden md:block">
+<nav  class="breadcrumbs text-xs font-semibold hidden md:block">
     <ul>
         @foreach ($crumbs as $crumb)
         <li>
             @if ($crumb->url && !$loop->last)
             <a href="{{ $crumb->url }}" class="inline-flex items-center gap-1">
-                {{-- icon --}}
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-folder-closed">
+                {{-- Icon folder --}}
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-folder-closed-icon lucide-folder-closed">
                     <path d="M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2Z" />
-                    <path d="M2 10h20" />
-                </svg>
+                    <path d="M2 10h20" /></svg>
                 {{ $crumb->title }}
             </a>
             @else
             <span class="inline-flex items-center gap-1">
+                {{-- Icon file --}}
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-folder-open-icon lucide-folder-open">
+                    <path d="m6 14 1.5-2.9A2 2 0 0 1 9.24 10H20a2 2 0 0 1 1.94 2.5l-1.54 6a2 2 0 0 1-1.95 1.5H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h3.9a2 2 0 0 1 1.69.9l.81 1.2a2 2 0 0 0 1.67.9H18a2 2 0 0 1 2 2v2" /></svg>
                 {{ $crumb->title }}
             </span>
             @endif
