fc8d6125b69551c63cf7959f704d50e6a0ae49a8
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-22 12:27:07 +0800

    sasa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Auth;
 use Diglactic\Breadcrumbs\Breadcrumbs;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,7 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        require base_path('routes/breadcrumbs.php');
+        if (file_exists(base_path('routes/breadcrumbs.php'))) {
+            require_once base_path('routes/breadcrumbs.php');
+        }
         App::setLocale(Session::get('locale', config('app.locale')));
 
         Blade::if('role', function ($roles) {
