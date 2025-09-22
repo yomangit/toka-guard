6769501bbac84a2ff19fab205308f5ed819e7962
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-22 14:28:31 +0800

    vcvcv

--- resources/views/components/manhours/layout.blade.php
+++ resources/views/components/manhours/layout.blade.php
@@ -1,4 +1,4 @@
-<div class="flex items-start max-md:flex-col ">
+<div class="flex items-start max-md:flex-col  z-30 ">
     <div class="flex-1 self-stretch max-md:pt-6">
         <flux:heading>{{ $heading ?? '' }}</flux:heading>
         <flux:subheading>{{ $subheading ?? '' }}</flux:subheading>
