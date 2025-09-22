22b25f05c6982b4ed0920216f58db151ad1a8353
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-22 12:16:02 +0800

    sa

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -6,7 +6,7 @@ use Diglactic\Breadcrumbs\Generator as Trail;
 // 1. Halaman daftar hazard
 Breadcrumbs::for('Hazard-List', function (Trail $trail) {
     $trail->push('Home', route('hazard')); // bisa diarahkan ke dashboard kalau perlu
-    $trail->push('Hazard List', route('hazard'));
+   
 });
 
 // 2. Form tambah hazard
