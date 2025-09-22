e8364fe48a3aa5fe1351aa384b0f15076e476fa7
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-23 07:58:30 +0800

    ssa

--- resources/views/livewire/administration/people/user.blade.php
+++ resources/views/livewire/administration/people/user.blade.php
@@ -70,156 +70,154 @@
                     </tbody>
                 </table>
             </div>
-            {{ $users->links() }}
+    </x-tabs-relation.layout>
+    {{ $users->links() }}
 
-            {{-- Create/Edit Modal --}}
-            <dialog class="modal" @if($showModal) open @endif>
-                <div class="modal-box w-11/12 max-w-2xl">
-                    <h3 class="font-bold text-lg">{{ $userId ? 'Edit User' : 'Add User' }}</h3>
+    {{-- Create/Edit Modal --}}
+    <dialog class="modal" @if($showModal) open @endif>
+        <div class="modal-box w-11/12 max-w-2xl">
+            <h3 class="font-bold text-lg">{{ $userId ? 'Edit User' : 'Add User' }}</h3>
 
-                    <div class="grid grid-cols-2 gap-4 mt-4">
-
-                        <fieldset class="fieldset">
-                            <label class="block">Nama</label>
-                            <input type="text" wire:model.live="name" class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" />
-                            <x-label-error :messages="$errors->get('name')" />
-                        </fieldset>
-
-                        <fieldset class="fieldset">
-                            <label class="block">Jenis Kelamin</label>
-                            <select wire:model.live="gender" class="select select-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs">
-                                <option value="">-- Pilih --</option>
-                                <option value="L">Laki - laki</option>
-                                <option value="P">Perempuan</option>
-                            </select>
-                            <x-label-error :messages="$errors->get('gender')" />
-                        </fieldset>
-
-                        <fieldset class="fieldset">
-                            <label class="block">Tanggal Lahir</label>
-                            <input type="text" readonly id="date_birth" wire:model="date_birth" class=" cursor-pointer input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" placeholder="Pilih tanggal lahir" x-data x-init="flatpickr($refs.input, {dateFormat: 'Y-m-d'})" x-ref="input" />
-                            <x-label-error :messages="$errors->get('date_birth')" />
-                        </fieldset>
-
-                        <fieldset class="fieldset">
-                            <label class="block">Username</label>
-                            <input type="text" wire:model.live="username" class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" />
-                            <x-label-error :messages="$errors->get('username')" />
-                        </fieldset>
-
-                        <fieldset class="fieldset">
-                            <label class="block">Department</label>
-                            <input type="text" wire:model.live="department_name" class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" />
-                            <x-label-error :messages="$errors->get('department_name')" />
-                        </fieldset>
-
-                        <fieldset class="fieldset">
-                            <label class="block">Employee ID</label>
-                            <input type="text" wire:model.live="employee_id" class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" />
-                            <x-label-error :messages="$errors->get('employee_id')" />
-                        </fieldset>
-
-                        <fieldset class="fieldset">
-                            <label class="block">Tanggal Masuk</label>
-                            <input type="text" readonly id="date_commenced" wire:model="date_commenced" class="cursor-pointer input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" placeholder="Pilih tanggal masuk" x-data x-init="flatpickr($refs.input, {dateFormat: 'Y-m-d'})" x-ref="input" />
-                            <x-label-error :messages="$errors->get('date_commenced')" />
-                        </fieldset>
-
-                        <fieldset class="fieldset">
-                            <label class="block">Email</label>
-                            <input type="email" wire:model.live="email" class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" />
-                            <x-label-error :messages="$errors->get('email')" />
-                        </fieldset>
-                        <fieldset class="fieldset">
-                            <label class="block">Jenis Kelamin</label>
-                            <select wire:model.live="role_id" class="select select-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs">
-                                <option value="">-- Pilih --</option>
-                                @foreach ($role as $role )
-                                <option value="{{ $role->id }}">{{$role->name}}</option>
-                                @endforeach
-                            </select>
-                            <x-label-error :messages="$errors->get('role_id')" />
-                        </fieldset>
-                    </div>
-
-                    <div class="modal-action">
-                        <flux:button wire:click="save" size="xs" icon:trailing="save" variant="primary"> {{ $userId ? 'Update' : 'Simpan' }}</flux:button>
-                        <flux:button size="xs" wire:click="$set('showModal', false)" icon:trailing="circle-x" variant="danger">Batal</flux:button>
-                    </div>
-                </div>
-            </dialog>
-
-
-            {{-- Delete Confirmation Modal --}}
-            <dialog class="modal" @if($showDeleteModal) open @endif>
-                <div class="modal-box">
-                    <h3 class="font-bold text-lg">Confirm Delete</h3>
-                    <p>Are you sure you want to delete this user?</p>
-                    <div class="modal-action">
-                        <button class="btn" wire:click="$set('showDeleteModal', false)">Cancel</button>
-                        <button class="btn btn-error" wire:click="delete">Delete</button>
-                    </div>
-                </div>
-            </dialog>
-        </div>
-
-        <dialog class="modal" @if($showImportModal) open @endif>
-            <div class="modal-box w-11/12 max-w-md">
-                <h3 class="font-bold text-lg">Import Users</h3>
+            <div class="grid grid-cols-2 gap-4 mt-4">
 
                 <fieldset class="fieldset">
-                    <label class="block">File Excel</label>
-                    <input type="file" wire:model.live="file" class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" />
-
-                    {{-- Error message --}}
-                    <x-label-error :messages="$errors->get('file')" />
-
-                    {{-- Loading indicator saat pilih file --}}
-                    <div wire:loading wire:target="file" class="text-info text-sm mt-1">
-                        ⏳ Sedang mengunggah file...
-                    </div>
+                    <label class="block">Nama</label>
+                    <input type="text" wire:model.live="name" class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" />
+                    <x-label-error :messages="$errors->get('name')" />
                 </fieldset>
-                @if (session()->has('success'))
-                <div class="alert alert-success my-2">
-                    {{ session('success') }}
-                </div>
-                @endif
-                <div class="modal-action">
-                    {{-- Tombol Import --}}
-                    <flux:button wire:click="import" size="xs" icon:trailing="save" variant="primary" wire:loading.attr="disabled" wire:target="import,file">
-
-                        <span wire:loading.remove wire:target="import,file">Import</span>
-                        <span wire:loading wire:target="import,file">Mengimpor...</span>
-                    </flux:button>
-
-                    {{-- Tombol Batal --}}
-                    <flux:button size="xs" wire:click="$set('showImportModal', false)" icon:trailing="circle-x" variant="danger">
-                        Batal
-                    </flux:button>
-                </div>
-            </div>
-        </dialog>
-
-        <dialog class="modal" @if($showBulkUpdateModal) open @endif>
-            <div class="modal-box">
-                <h3 class="font-bold text-lg">Bulk Update User</h3>
 
                 <fieldset class="fieldset">
-                    <label class="block">Role Baru</label>
-                    <select wire:model="bulkRole" class="select select-bordered w-full input-xs">
-                        <option value="">-- Pilih Role --</option>
-                        @foreach ($roles as $r)
-                        <option value="{{ $r->id }}">{{ $r->name }}</option>
+                    <label class="block">Jenis Kelamin</label>
+                    <select wire:model.live="gender" class="select select-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs">
+                        <option value="">-- Pilih --</option>
+                        <option value="L">Laki - laki</option>
+                        <option value="P">Perempuan</option>
+                    </select>
+                    <x-label-error :messages="$errors->get('gender')" />
+                </fieldset>
+
+                <fieldset class="fieldset">
+                    <label class="block">Tanggal Lahir</label>
+                    <input type="text" readonly id="date_birth" wire:model="date_birth" class=" cursor-pointer input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" placeholder="Pilih tanggal lahir" x-data x-init="flatpickr($refs.input, {dateFormat: 'Y-m-d'})" x-ref="input" />
+                    <x-label-error :messages="$errors->get('date_birth')" />
+                </fieldset>
+
+                <fieldset class="fieldset">
+                    <label class="block">Username</label>
+                    <input type="text" wire:model.live="username" class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" />
+                    <x-label-error :messages="$errors->get('username')" />
+                </fieldset>
+
+                <fieldset class="fieldset">
+                    <label class="block">Department</label>
+                    <input type="text" wire:model.live="department_name" class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" />
+                    <x-label-error :messages="$errors->get('department_name')" />
+                </fieldset>
+
+                <fieldset class="fieldset">
+                    <label class="block">Employee ID</label>
+                    <input type="text" wire:model.live="employee_id" class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" />
+                    <x-label-error :messages="$errors->get('employee_id')" />
+                </fieldset>
+
+                <fieldset class="fieldset">
+                    <label class="block">Tanggal Masuk</label>
+                    <input type="text" readonly id="date_commenced" wire:model="date_commenced" class="cursor-pointer input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" placeholder="Pilih tanggal masuk" x-data x-init="flatpickr($refs.input, {dateFormat: 'Y-m-d'})" x-ref="input" />
+                    <x-label-error :messages="$errors->get('date_commenced')" />
+                </fieldset>
+
+                <fieldset class="fieldset">
+                    <label class="block">Email</label>
+                    <input type="email" wire:model.live="email" class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" />
+                    <x-label-error :messages="$errors->get('email')" />
+                </fieldset>
+                <fieldset class="fieldset">
+                    <label class="block">Jenis Kelamin</label>
+                    <select wire:model.live="role_id" class="select select-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs">
+                        <option value="">-- Pilih --</option>
+                        @foreach ($role as $role )
+                        <option value="{{ $role->id }}">{{$role->name}}</option>
                         @endforeach
                     </select>
+                    <x-label-error :messages="$errors->get('role_id')" />
                 </fieldset>
-                <div class="modal-action">
-                    <flux:button size="xs" wire:click="bulkUpdate" variant="primary">Update</flux:button>
-                    <flux:button size="xs" wire:click="$set('showBulkUpdateModal', false)" variant="danger">Batal</flux:button>
-                </div>
             </div>
-        </dialog>
+
+            <div class="modal-action">
+                <flux:button wire:click="save" size="xs" icon:trailing="save" variant="primary"> {{ $userId ? 'Update' : 'Simpan' }}</flux:button>
+                <flux:button size="xs" wire:click="$set('showModal', false)" icon:trailing="circle-x" variant="danger">Batal</flux:button>
+            </div>
+        </div>
+    </dialog>
 
 
-    </x-tabs-relation.layout>
+    {{-- Delete Confirmation Modal --}}
+    <dialog class="modal" @if($showDeleteModal) open @endif>
+        <div class="modal-box">
+            <h3 class="font-bold text-lg">Confirm Delete</h3>
+            <p>Are you sure you want to delete this user?</p>
+            <div class="modal-action">
+                <button class="btn" wire:click="$set('showDeleteModal', false)">Cancel</button>
+                <button class="btn btn-error" wire:click="delete">Delete</button>
+            </div>
+        </div>
+    </dialog>
+    </div>
+
+    <dialog class="modal" @if($showImportModal) open @endif>
+        <div class="modal-box w-11/12 max-w-md">
+            <h3 class="font-bold text-lg">Import Users</h3>
+
+            <fieldset class="fieldset">
+                <label class="block">File Excel</label>
+                <input type="file" wire:model.live="file" class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" />
+
+                {{-- Error message --}}
+                <x-label-error :messages="$errors->get('file')" />
+
+                {{-- Loading indicator saat pilih file --}}
+                <div wire:loading wire:target="file" class="text-info text-sm mt-1">
+                    ⏳ Sedang mengunggah file...
+                </div>
+            </fieldset>
+            @if (session()->has('success'))
+            <div class="alert alert-success my-2">
+                {{ session('success') }}
+            </div>
+            @endif
+            <div class="modal-action">
+                {{-- Tombol Import --}}
+                <flux:button wire:click="import" size="xs" icon:trailing="save" variant="primary" wire:loading.attr="disabled" wire:target="import,file">
+
+                    <span wire:loading.remove wire:target="import,file">Import</span>
+                    <span wire:loading wire:target="import,file">Mengimpor...</span>
+                </flux:button>
+
+                {{-- Tombol Batal --}}
+                <flux:button size="xs" wire:click="$set('showImportModal', false)" icon:trailing="circle-x" variant="danger">
+                    Batal
+                </flux:button>
+            </div>
+        </div>
+    </dialog>
+
+    <dialog class="modal" @if($showBulkUpdateModal) open @endif>
+        <div class="modal-box">
+            <h3 class="font-bold text-lg">Bulk Update User</h3>
+
+            <fieldset class="fieldset">
+                <label class="block">Role Baru</label>
+                <select wire:model="bulkRole" class="select select-bordered w-full input-xs">
+                    <option value="">-- Pilih Role --</option>
+                    @foreach ($roles as $r)
+                    <option value="{{ $r->id }}">{{ $r->name }}</option>
+                    @endforeach
+                </select>
+            </fieldset>
+            <div class="modal-action">
+                <flux:button size="xs" wire:click="bulkUpdate" variant="primary">Update</flux:button>
+                <flux:button size="xs" wire:click="$set('showBulkUpdateModal', false)" variant="danger">Batal</flux:button>
+            </div>
+        </div>
+    </dialog>
 </section>
