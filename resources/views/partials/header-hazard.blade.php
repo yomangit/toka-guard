ee3562ee50a028d9f0640f69a51c3e2d91f0cf1b
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-22 12:42:49 +0800

    fdf

--- resources/views/partials/header-hazard.blade.php
+++ resources/views/partials/header-hazard.blade.php
@@ -1,5 +1,5 @@
+<div> {{ Breadcrumbs::render('hazard') }}</div>
 <div class="relative mb-3 w-full">
-    <div> {{ Breadcrumbs::render('hazard') }}</div>
     <flux:heading level="1" class="mb-1 capitalize"> {{ __('Hazard') }}</flux:heading>
     <flux:separator variant="subtle" />
 </div>
