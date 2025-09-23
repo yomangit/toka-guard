e6de38fb4ac9ec5066ee2dd91ed3261d381531f9
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-23 10:42:03 +0800

    vc

--- resources/views/livewire/hazard/hazard-list.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- resources/views/livewire/hazard-list.blade.php -->
-<section class="w-full">
-    
-
-    <x-manhours.layout>
-        <livewire:hazard.hazard-report-panel />
-    </x-manhours.layout>
-
-</section>
