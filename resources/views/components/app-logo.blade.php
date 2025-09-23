8a1b991d9abe5cb640602aa0e455b651d6fe1d2b
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-23 13:53:09 +0800

    ASDs

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -2,6 +2,6 @@
     <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
-    <span class=" truncate leading-tight font-semibold">Tosar</span>
+    <span class=" truncate leading-tight font-semibold">Tokasafe</span>
     <span class="mb-0.5 truncate leading-tight font-extralight text-[8px]">Toka Safe & Reporting</span>
 </div>
