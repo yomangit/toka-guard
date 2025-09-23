3755221362e96f76ed8b5204fce030eb9f46f94d
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-23 08:04:43 +0800

    asdfa

--- app/Policies/HazardPolicy.php
+++ app/Policies/HazardPolicy.php
@@ -26,28 +26,24 @@ class HazardPolicy
         if ($user->roles()->where('role_id', 1)->exists()) {
             return true;
         }
-
         // Penanggung jawab bisa
-        // elseif ($hazard->penanggungJawab && $user->id === $hazard->penanggungJawab->id) {
-        //     return true;
-        // }
-
+        elseif ($hazard->penanggungJawab && $user->id === $hazard->penanggungJawab->id) {
+            return true;
+        }
         // Pelapor bisa
         elseif ($hazard->pelapor && $user->id === $hazard->pelapor->id) {
             return true;
         }
-
         // Assigned ERM atau moderator sesuai event_type
         elseif (DB::table('hazard_erm_assignments')
             ->where('hazard_id', $hazard->id)
             ->where('erm_id', $user->id) // Asumsi user.id adalah yang dicocokkan dengan erm_id
-            ->exists()) {
+            ->exists()
+        ) {
             return true;
-        } 
-        // elseif ($user->moderatorAssignments()->where('event_type_id', $hazard->event_type_id)->exists()) {
-        //     return true;
-        // } 
-        else {
+        } elseif ($user->moderatorAssignments()->where('event_type_id', $hazard->event_type_id)->exists()) {
+            return true;
+        } else {
             return false;
         }
     }
@@ -65,7 +61,17 @@ class HazardPolicy
      */
     public function update(User $user, Hazard $hazard): bool
     {
-        return false;
+        if ($user->roles()->where('role_id', 1)->exists()) {
+            return true;
+        } elseif (DB::table('hazard_erm_assignments')
+            ->where('hazard_id', $hazard->id)
+            ->where('erm_id', $user->id) // Asumsi user.id adalah yang dicocokkan dengan erm_id
+            ->exists()
+        ) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
