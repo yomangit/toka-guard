9457f150be27fe5692341f0de36c83fc9c264b45
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-23 13:54:42 +0800

    fasfd

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,4 +1,4 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-full bg-accent-content text-accent-foreground">
+<div class="flex aspect-square size-10 items-center justify-center rounded-full bg-accent-content text-accent-foreground">
     <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
