637f3582599d263dfaab9ca3c532220f1034d65c
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-24 20:36:18 +0800

    sdsd

--- resources/views/livewire/hazard/hazard-report-panel.blade.php
+++ resources/views/livewire/hazard/hazard-report-panel.blade.php
@@ -129,7 +129,7 @@
                     <th class="border px-2 py-1">Status</th>
                     <th class="border px-2 py-1">Pelapor</th>
                     <th class="border px-2 py-1">Tanggal</th>
-                   <th class="flex-col border px-2 py-1">
+                   <th class="flex-col border px-2 py-1 text-center">
                         <p>Action</p>
                         <p>Total/Open</p>
                     </th>
