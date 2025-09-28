72b116b9fecb8532236026444005475ef5002ef7
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-28 15:22:11 +0700

    asdfasdf

--- resources/views/livewire/hazard/hazard-report-panel.blade.php
+++ resources/views/livewire/hazard/hazard-report-panel.blade.php
@@ -129,21 +129,23 @@
                     <th class="border">Divisi Penanggung Jawab
                         <button class="btn btn-ghost btn-xs" popovertarget="divisi_dept" style="anchor-name:--divisi_dept">
                             {{-- Ikon Filter: Tampilkan jika filterDepartment tidak kosong --}}
-                            @if(empty($filterDepartment) && empty($filterContractor))
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-list-filter-icon lucide-list-filter">
-                                <path d="M2 5h20" />
-                                <path d="M6 12h12" />
-                                <path d="M9 19h6" />
-                            </svg>
-                            @else
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-down-wide-narrow-icon lucide-arrow-down-wide-narrow">
-                                <path d="m3 16 4 4 4-4" />
-                                <path d="M7 20V4" />
-                                <path d="M11 4h10" />
-                                <path d="M11 8h7" />
-                                <path d="M11 12h4" />
-                            </svg>
-                            @endif
+                            <span class="text-blue-600 text-xs">
+                                @if(empty($filterDepartment) && empty($filterContractor))
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-list-filter-icon lucide-list-filter">
+                                    <path d="M2 5h20" />
+                                    <path d="M6 12h12" />
+                                    <path d="M9 19h6" />
+                                </svg>
+                                @else
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-down-wide-narrow-icon lucide-arrow-down-wide-narrow">
+                                    <path d="m3 16 4 4 4-4" />
+                                    <path d="M7 20V4" />
+                                    <path d="M11 4h10" />
+                                    <path d="M11 8h7" />
+                                    <path d="M11 12h4" />
+                                </svg>
+                                @endif
+                            </span>
                         </button>
                         <ul class="dropdown menu w-52 rounded-box bg-base-100 shadow-lg p-2 max-h-60 overflow-y-auto" popover id="divisi_dept" style="position-anchor:--divisi_dept; inset-area: bottom span-right;">
                             {{-- Loop Department --}}
@@ -170,13 +172,23 @@
                     <th class="border relative">
                         Status
                         <button class="btn btn-ghost btn-xs" popovertarget="popover-1" style="anchor-name:--anchor-1">
+
                             <span class="text-blue-600 text-xs">
-                                {{-- SVG Icon --}}
+                                @if(empty($filterStatus))
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-list-filter-icon lucide-list-filter">
                                     <path d="M2 5h20" />
                                     <path d="M6 12h12" />
                                     <path d="M9 19h6" />
                                 </svg>
+                                @else
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-down-wide-narrow-icon lucide-arrow-down-wide-narrow">
+                                    <path d="m3 16 4 4 4-4" />
+                                    <path d="M7 20V4" />
+                                    <path d="M11 4h10" />
+                                    <path d="M11 8h7" />
+                                    <path d="M11 12h4" />
+                                </svg>
+                                @endif
                             </span>
                         </button>
                         {{-- Dropdown Menu --}}
