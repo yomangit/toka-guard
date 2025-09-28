c5ca2b62b3fc3ddea62a9151a368710a13843e72
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-28 12:11:06 +0800

    sas

--- app/Livewire/Hazard/HazardReportPanel.php
+++ app/Livewire/Hazard/HazardReportPanel.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Auth;
 class HazardReportPanel extends Component
 {
     use WithPagination;
-    public $filterStatus = 'all', $role;
+    public $filterStatus = [], $role;
     public $filterEventType;
     public $filterEventSubType;
     public $filterDepartment;
