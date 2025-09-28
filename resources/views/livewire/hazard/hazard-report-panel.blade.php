5c9a493194bab5323e8dcd2945c1105f93971d8c
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-28 15:20:24 +0700

    sas

--- resources/views/livewire/hazard/hazard-report-panel.blade.php
+++ resources/views/livewire/hazard/hazard-report-panel.blade.php
@@ -129,7 +129,7 @@
                     <th class="border">Divisi Penanggung Jawab
                         <button class="btn btn-ghost btn-xs" popovertarget="divisi_dept" style="anchor-name:--divisi_dept">
                             {{-- Ikon Filter: Tampilkan jika filterDepartment tidak kosong --}}
-                            @if(empty($filterDepartment) || empty($filterContractor))
+                            @if(empty($filterDepartment) && empty($filterContractor))
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-list-filter-icon lucide-list-filter">
                                 <path d="M2 5h20" />
                                 <path d="M6 12h12" />
