d76042733db41d9997eb92125e779e625da7cad8
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-28 15:42:01 +0700

    asdfasdf

--- resources/views/livewire/hazard/hazard-report-panel.blade.php
+++ resources/views/livewire/hazard/hazard-report-panel.blade.php
@@ -128,7 +128,7 @@
                     <th class="border">Jenis Bahaya
                         <button class="btn btn-ghost btn-xs" popovertarget="eventSubType" style="anchor-name:--eventSubType">
                             <span class="text-blue-600 text-xs">
-                                @if(empty($filterDepartment) && empty($filterContractor))
+                                @if(empty($filterEventSubType))
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-list-filter-icon lucide-list-filter">
                                     <path d="M2 5h20" />
                                     <path d="M6 12h12" />
@@ -150,7 +150,7 @@
                             @foreach ($filterOptions['EventSubType'] as $event_sub_type)
                             <li>
                                 <label class="flex items-center cursor-pointer hover:bg-gray-100 p-1 rounded">
-                                    <input type="checkbox" wire:model.live="filterDepartment" value="{{ $event_sub_type->id }}" class="form-checkbox text-blue-600 rounded">
+                                    <input type="checkbox" wire:model.live="filterEventSubType" value="{{ $event_sub_type->id }}" class="form-checkbox text-blue-600 rounded">
                                     <span class="ml-2 text-xs capitalize">{{ $event_sub_type->event_sub_type_name }}</span>
                                 </label>
                             </li>
