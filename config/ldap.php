ac2ce9d9ba4302e715a9f4fb01cd38fba40bde38
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-29 12:43:00 +0700

    adasd

--- config/ldap.php
+++ config/ldap.php
@@ -26,24 +26,18 @@ return [
     |
     */
 
-    'connections' => [
-
-        'default' => [
-            'hosts' => [env('LDAP_HOST', '127.0.0.1')],
-            'username' => env('LDAP_USERNAME', 'cn=user,dc=local,dc=com'),
-            'password' => env('LDAP_PASSWORD', 'secret'),
-            'port' => env('LDAP_PORT', 389),
-            'base_dn' => env('LDAP_BASE_DN', 'dc=local,dc=com'),
-            'timeout' => env('LDAP_TIMEOUT', 5),
-            'use_ssl' => env('LDAP_SSL', false),
-            'use_tls' => env('LDAP_TLS', false),
-            'use_sasl' => env('LDAP_SASL', false),
-            'sasl_options' => [
-                // 'mech' => 'GSSAPI',
-            ],
-        ],
-
+'connections' => [
+    'default' => [
+        'hosts'    => ['adserver.archimining.local'], // atau IP
+        'username' => null,   // <- kosong, tidak pakai service account
+        'password' => null,
+        'port'     => 389,    // 636 jika LDAPS
+        'use_ssl'  => false,
+        'use_tls'  => false,
+        'base_dn'  => 'DC=archimining,DC=local',
     ],
+],
+
 
     /*
     |--------------------------------------------------------------------------
