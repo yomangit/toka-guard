d69a3d8d5fbec078978066f7365968b6d2f2e6d0
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-10-06 13:16:48 +0800

    sa

--- resources/views/flux/input/index.blade.php
+++ resources/views/flux/input/index.blade.php
@@ -78,7 +78,7 @@ $inputLoadingClasses = Flux::classes()
     ;
 
 $classes = Flux::classes()
-    ->add('w-full max-w-sm border rounded-xs dark:outline-none block disabled:shadow-none dark:shadow-none ring-0 focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden')
+    ->add('input input-bordered w-full max-w-sm   block disabled:shadow-none dark:shadow-none ring-0 focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden')
     ->add('appearance-none') // Without this, input[type="date"] on mobile doesn't respect w-full...
     ->add(match ($size) {
         default => 'text-accent sm:text-sm py-2 h-10 leading-[1.375rem]', // This makes the height of the input 40px (same as buttons and such...)
