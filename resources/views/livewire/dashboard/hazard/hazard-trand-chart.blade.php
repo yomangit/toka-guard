e20eb4cbde227ad596b9443216e75a42d3af4b84
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-10-07 13:59:58 +0800

    ASD

--- resources/views/livewire/dashboard/hazard/hazard-trand-chart.blade.php
+++ resources/views/livewire/dashboard/hazard/hazard-trand-chart.blade.php
@@ -12,44 +12,97 @@
         var option;
 
         option = {
-            title: {
-                text: 'Stacked Line'
-            }
-            , textStyle: {
+            textStyle: {
                 fontFamily: 'Microsoft YaHei'
                 , fontSize: 12
                 , fontStyle: 'normal'
                 , fontWeight: 'normal'
             , }
+            , grid: {
+                top: 50
+                , right: 30
+                , bottom: 50
+                , left: 50
+                , containLabel: true
+            }
+            , tooltip: {
+                trigger: 'axis'
+                , backgroundColor: 'rgba(50,50,50,0.8)'
+                , borderWidth: 0
+                , textStyle: {
+                    color: '#fff'
+                    , fontFamily: 'Microsoft YaHei'
+                    , fontSize: 12
+                , }
+            }
+            , legend: {
+                data: ['Jumlah Laporan']
+                , top: 10
+                , left: 'center'
+                , textStyle: {
+                    fontFamily: 'Microsoft YaHei'
+                    , fontSize: 12
+                    , fontWeight: 'normal'
+                }
+            }
             , xAxis: {
                 type: 'category'
                 , data: data.months
+                , axisLine: {
+                    lineStyle: {
+                        color: '#888'
+                    }
+                }
                 , axisLabel: {
                     fontFamily: 'Microsoft YaHei'
                     , fontSize: 12
-                    , fontStyle: 'normal'
-                    , fontWeight: 'normal'
-                , }
+                }
+                , axisTick: {
+                    show: false
+                }
             }
             , yAxis: {
                 type: 'value'
+                , axisLine: {
+                    lineStyle: {
+                        color: '#888'
+                    }
+                }
+                , splitLine: {
+                    lineStyle: {
+                        type: 'dashed'
+                        , color: '#ddd'
+                    }
+                }
                 , axisLabel: {
                     fontFamily: 'Microsoft YaHei'
                     , fontSize: 12
-                    , fontStyle: 'normal'
-                    , fontWeight: 'normal'
-                , }
+                }
             }
             , series: [{
-                data: data.counts
+                name: 'Jumlah Laporan'
+                , data: data.counts
                 , type: 'line'
                 , smooth: true
+                , lineStyle: {
+                    width: 3
+                }
+                , symbol: 'circle'
+                , symbolSize: 6
+                , itemStyle: {
+                    color: '#3B82F6'
+                }
+                , areaStyle: {
+                    opacity: 0.1
+                    , color: '#3B82F6'
+                }
             }]
         };
 
         if (option && typeof option === 'object') {
             myChart.setOption(option);
         }
+
 
         window.addEventListener('resize', myChart.resize);
 
