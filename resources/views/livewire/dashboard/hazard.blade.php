ce49b2239c863477fb973cef6cfd1cf691727209
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-10-07 13:00:06 +0800

    sasas

--- resources/views/livewire/dashboard/hazard.blade.php
+++ resources/views/livewire/dashboard/hazard.blade.php
@@ -37,16 +37,7 @@
                 <div class="stat-value text-secondary">2.6M</div>
                 <div class="stat-desc">21% more than last month</div>
             </div>
-            <div class="stat ">
-                <div class="stat-figure text-secondary">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-8 w-8 stroke-current">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path>
-                    </svg>
-                </div>
-                <div class="stat-title">Overdue</div>
-                <div class="stat-value text-secondary">2.6M</div>
-                <div class="stat-desc">21% more than last month</div>
-            </div>
+            
         </div>
 
         {{-- Grafik --}}
