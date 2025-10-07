fb9124035c26a0ae6059472b9afe1d0f62b44f9d
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-10-07 13:03:17 +0800

    asdf

--- resources/views/livewire/dashboard/hazard/hazard-trand-chart.blade.php
+++ resources/views/livewire/dashboard/hazard/hazard-trand-chart.blade.php
@@ -1,38 +1,35 @@
-<div class=" bg-white p-4 rounded-lg shadow">
     <div wire:ignore id="hazardTrend" style="height: 250px;" class="w-full"></div>
-</div>
+    @push('scripts')
+    <!-- Load ECharts dari CDN -->
+    <script src="https://echarts.apache.org/en/js/vendors/echarts/dist/echarts.min.js"></script>
+    <script src="https://echarts.apache.org/en/js/vendors/echarts-gl/dist/echarts-gl.min.js"></script>
 
-@push('scripts')
-<!-- Load ECharts dari CDN -->
-<script src="https://echarts.apache.org/en/js/vendors/echarts/dist/echarts.min.js"></script>
-<script src="https://echarts.apache.org/en/js/vendors/echarts-gl/dist/echarts-gl.min.js"></script>
+    <script>
+        const data = JSON.parse('<?php echo $data ?>');
+        console.log(data);
+        var dom = document.getElementById('hazardTrend');
+        var myChart = echarts.init(dom);
+        var option;
 
-<script>
-    const data = JSON.parse('<?php echo $data ?>');
-    console.log(data);
-    var dom = document.getElementById('hazardTrend');
-    var myChart = echarts.init(dom);
-    var option;
+        option = {
+            xAxis: {
+                type: 'category'
+                , data: data.months
+            }
+            , yAxis: {
+                type: 'value'
+            }
+            , series: [{
+                data: data.counts
+                , type: 'line'
+            }]
+        };
 
-    option = {
-        xAxis: {
-            type: 'category'
-            , data: data.months
+        if (option && typeof option === 'object') {
+            myChart.setOption(option);
         }
-        , yAxis: {
-            type: 'value'
-        }
-        , series: [{
-            data: data.counts
-            , type: 'line'
-        }]
-    };
 
-    if (option && typeof option === 'object') {
-        myChart.setOption(option);
-    }
+        window.addEventListener('resize', myChart.resize);
 
-    window.addEventListener('resize', myChart.resize);
-
-</script>
-@endpush
+    </script>
+    @endpush
