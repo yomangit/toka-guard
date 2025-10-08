fd3bac12a927e40e29ddf0c1b1a691b28a553104
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-10-08 08:01:09 +0800

    re

--- resources/views/livewire/dashboard/hazard.blade.php
+++ resources/views/livewire/dashboard/hazard.blade.php
@@ -41,7 +41,7 @@
         </div>
 
         {{-- Grafik --}}
-        <div class="grid grid-cols-1 lg:grid-cols-3 gap-6 my-2">
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-2 my-2">
             <div class="bg-white rounded-xl lg:col-span-2 shadow">
                 <livewire:dashboard.hazard.hazard-trand-chart />
             </div>
