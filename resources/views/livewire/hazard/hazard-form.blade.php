6b2eea83c2753f39c0e6a21b37f18aa8d819b473
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-10-10 15:28:50 +0800

    dssd

--- resources/views/livewire/hazard/hazard-form.blade.php
+++ resources/views/livewire/hazard/hazard-form.blade.php
@@ -42,7 +42,7 @@
                                         <x-form.label for="kta" class="peer-checked/kta:text-accent text-[10px]" label="Kondisi Tidak Aman" required />
                                         <input id="tta" value="tta" wire:model.live="keyWord" class="peer/tta radio radio-xs radio-primary" type="radio" name="keyWord" />
                                         <x-form.label for="tta" class="peer-checked/tta:text-primary text-[10px]" label="Tindakan Tidak Aman" required />
-                                        <div class="hidden peer-checked/kta:block mt-1">
+                                        <div class="hidden peer-checked/kta:block mt-1.5">
                                             <select wire:model.live="kondisi_tidak_aman" class="select select-xs mb-1 select-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden {{ $errors->has('kondisi_tidak_aman') ? 'ring-1 ring-rose-500 focus:ring-rose-500 focus:border-rose-500' : '' }}">
                                                 <option value="">-- Pilih Kondisi Tidak Aman --</option>
                                                 @foreach ($ktas as $kta)
@@ -51,7 +51,7 @@
                                             </select>
     
                                         </div>
-                                        <div class="hidden peer-checked/tta:block mt-1">
+                                        <div class="hidden peer-checked/tta:block mt-1.5">
                                             <select wire:model.live="tindakan_tidak_aman" class="select select-xs mb-1 select-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden {{ $errors->has('tindakan_tidak_aman') ? 'ring-1 ring-rose-500 focus:ring-rose-500 focus:border-rose-500' : '' }}">
                                                 <option value="">-- Pilih Tidakan Tidak Aman --</option>
                                                 @foreach ($ttas as $tta)
