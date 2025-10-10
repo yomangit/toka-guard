4655ac4e6607a1fb23e208186868f38cf902a32b
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-10-10 13:51:28 +0800

    ds

--- app/Livewire/Dashboard/Hazard.php
+++ app/Livewire/Dashboard/Hazard.php
@@ -17,7 +17,7 @@ class Hazard extends Component
         // Cek apakah nilai tidak kosong
         if (!empty($value)) {
             // Pisahkan string berdasarkan " to "
-            $dates = explode(' to ', $value);
+            $dates = explode(' ke ', $value);
 
             // Pastikan ada dua tanggal yang valid
             if (count($dates) === 2) {
