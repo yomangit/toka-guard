f153ddf8bd51056c5aa7150d07fc41d2bafc2672
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-10-10 13:16:33 +0800

    a

--- resources/views/livewire/dashboard/hazard/hazard-distribusi-status.blade.php
+++ resources/views/livewire/dashboard/hazard/hazard-distribusi-status.blade.php
@@ -6,7 +6,7 @@
         var dom_status = document.getElementById('chart-container');
         // 🧠 Ambil data dari Livewire (JSON string → object JS)
         // const chartData = JSON.parse('<?php echo $statusChart ?>');
-        const chartData = @json($pelapor);
+        const chartData = @json($statusChart);
         const labels = chartData.labels;
         const values = chartData.values;
         // 🔥 Masukkan data Livewire ke format yang ECharts butuh
