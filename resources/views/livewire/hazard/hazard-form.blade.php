8481dd7305c49390128192cf9e84b677ea348602
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-10-16 16:05:57 +0800

    sas

--- resources/views/livewire/hazard/hazard-form.blade.php
+++ resources/views/livewire/hazard/hazard-form.blade.php
@@ -588,11 +588,11 @@
             </table>
             @endif
             <div class="hidden md:block flex justify-end mt-4">
-                <flux:button size="xs" type="submit" icon:trailing="send" variant="primary">Kirim Laporan
+                <flux:button size="xs" type="submit" onclick="return validateDescription()" icon:trailing="send" variant="primary">Kirim Laporan
                 </flux:button>
             </div>
             <div class="block md:hidden  mt-4">
-                <flux:button size="xs" class="w-full" type="submit" icon:trailing="send" variant="primary">
+                <flux:button size="xs" class="w-full" type="submit" onclick="return validateDescription()" icon:trailing="send" variant="primary">
                     Kirim Laporan</flux:button>
             </div>
         </form>
