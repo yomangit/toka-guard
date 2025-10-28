60dc82cc6f5da1414c004166daf8916996d9e2d7
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-10-28 10:30:23 +0800

    sassdasd

--- resources/views/livewire/hazard/import-hazard-reports-modal.blade.php
+++ resources/views/livewire/hazard/import-hazard-reports-modal.blade.php
@@ -1,49 +1,34 @@
 <div>
-    <!-- Tombol trigger modal -->
-    <x-flux.modal.trigger name="importHazardModal">
-        <x-flux.button icon="upload" class="btn-primary">
-            Import Excel
-        </x-flux.button>
-    </x-flux.modal.trigger>
+    <!-- Tombol untuk membuka modal -->
+    <x-flux:button icon="upload" wire:click="$set('showModal', true)" class="btn-primary">
+        Import Excel
+    </x-flux:button>
 
     <!-- Modal -->
-    <x-flux.modal name="importHazardModal" size="md">
-        <x-slot name="title">
-            Import Data Hazard Reports
-        </x-slot>
+    <x-flux:modal wire:model="showModal" title="Import Data Hazard Reports" size="md">
+        <div class="space-y-4">
+            <p class="text-sm text-gray-600">
+                Unggah file Excel (.xlsx / .csv) sesuai format header hazard report.
+            </p>
 
-        <x-slot name="content">
-            <div class="space-y-4">
-                <p class="text-sm text-gray-600">
-                    Unggah file Excel (.xlsx / .csv) sesuai format kolom hazard report.
-                </p>
+            <div>
+                <input type="file" wire:model="file" accept=".xlsx,.xls,.csv"
+                       class="file-input file-input-bordered w-full border-gray-300" />
 
-                <div>
-                    <input type="file" wire:model="file" accept=".xlsx,.xls,.csv"
-                           class="file-input file-input-bordered w-full border-gray-300" />
+                @error('file')
+                    <p class="text-sm text-red-500 mt-1">{{ $message }}</p>
+                @enderror
 
-                    @error('file')
-                        <p class="text-sm text-red-500 mt-1">{{ $message }}</p>
-                    @enderror
-
-                    <div wire:loading wire:target="file" class="text-sm text-blue-500 mt-2">
-                        Mengunggah file...
-                    </div>
+                <div wire:loading wire:target="file" class="text-sm text-blue-500 mt-2">
+                    Mengunggah file...
                 </div>
             </div>
-        </x-slot>
+        </div>
 
         <x-slot name="footer">
             <div class="flex justify-end gap-2">
-                <x-flux.modal.close>
-                    <x-flux.button flat>Batal</x-flux.button>
-                </x-flux.modal.close>
+                <x-flux:button variant="ghost" wire:click="$set('showModal', false)">
+                    Batal
+                </x-flux:button>
 
-                <x-flux.button wire:click="import" wire:loading.attr="disabled" primary>
-                    <span wire:loading.remove>Import</span>
-                    <span wire:loading>Memproses...</span>
-                </x-flux.button>
-            </div>
-        </x-slot>
-    </x-flux.modal>
-</div>
+                <x-flux:button variant="primary" wire:click="import" wire:loading.attr="disabled">
