b3298a2a9752abf3ddc429834426edc3c72b35da
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-10-28 10:27:41 +0800

    sasa

--- resources/views/livewire/hazard/import-hazard-reports-modal.blade.php
+++ resources/views/livewire/hazard/import-hazard-reports-modal.blade.php
@@ -1,11 +1,13 @@
 <div>
-    <!-- Tombol Buka Modal -->
-    <x-button wire:click="openModal" icon="upload" class="btn-primary">
-        Import Excel
-    </x-button>
+    <!-- Tombol trigger modal -->
+    <x-flux.modal.trigger name="importHazardModal">
+        <x-flux.button icon="upload" class="btn-primary">
+            Import Excel
+        </x-flux.button>
+    </x-flux.modal.trigger>
 
-    <!-- Modal Upload -->
-    <flux:modal wire:model="open" size="md">
+    <!-- Modal -->
+    <x-flux.modal name="importHazardModal" size="md">
         <x-slot name="title">
             Import Data Hazard Reports
         </x-slot>
@@ -13,18 +15,18 @@
         <x-slot name="content">
             <div class="space-y-4">
                 <p class="text-sm text-gray-600">
-                    Unggah file Excel (.xlsx / .csv) sesuai format header hazard report.
+                    Unggah file Excel (.xlsx / .csv) sesuai format kolom hazard report.
                 </p>
 
                 <div>
                     <input type="file" wire:model="file" accept=".xlsx,.xls,.csv"
-                        class="file-input file-input-bordered w-full" />
+                           class="file-input file-input-bordered w-full border-gray-300" />
 
                     @error('file')
                         <p class="text-sm text-red-500 mt-1">{{ $message }}</p>
                     @enderror
 
-                    <div wire:loading wire:target="file" class="text-sm text-info mt-2">
+                    <div wire:loading wire:target="file" class="text-sm text-blue-500 mt-2">
                         Mengunggah file...
                     </div>
                 </div>
@@ -32,12 +34,16 @@
         </x-slot>
 
         <x-slot name="footer">
-            <x-button flat wire:click="closeModal" wire:loading.attr="disabled">Batal</x-button>
+            <div class="flex justify-end gap-2">
+                <x-flux.modal.close>
+                    <x-flux.button flat>Batal</x-flux.button>
+                </x-flux.modal.close>
 
-            <x-button primary wire:click="import" wire:loading.attr="disabled">
-                <span wire:loading.remove>Import</span>
-                <span wire:loading>Memproses...</span>
-            </x-button>
+                <x-flux.button wire:click="import" wire:loading.attr="disabled" primary>
+                    <span wire:loading.remove>Import</span>
+                    <span wire:loading>Memproses...</span>
+                </x-flux.button>
+            </div>
         </x-slot>
-    </flux:modal>
+    </x-flux.modal>
 </div>
