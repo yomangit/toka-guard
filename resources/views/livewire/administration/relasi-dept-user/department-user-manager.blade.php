ddef03cdba8313fefdd308aad16bfcc01cf36e25
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-10-31 15:03:30 +0800

    SA

--- resources/views/livewire/administration/relasi-dept-user/department-user-manager.blade.php
+++ resources/views/livewire/administration/relasi-dept-user/department-user-manager.blade.php
@@ -41,7 +41,11 @@
                     <span class="text-xs">Hanya terpilih</span>
                 </label>
             </div>
-            {{ $selectedUsers }}
+            <ul>
+                @foreach ($selectedUsers as $userId)
+                <li>{{ \App\Models\User::find($userId)?->name }}</li>
+                @endforeach
+            </ul>
             <div class="grid grid-cols-1 md:grid-cols-2 gap-2 mt-1">
                 @foreach($users as $user)
                 @if(!$showOnlySelected || in_array($user->id, $selectedUsers))
@@ -53,7 +57,7 @@
                 @endif
                 @endforeach
             </div>
-              <div class="mt-4">{{ $users->links() }}</div>
+            <div class="mt-4">{{ $users->links() }}</div>
         </fieldset>
 
         <button wire:click="save" class="btn btn-sm btn-success">Simpan Relasi</button>
