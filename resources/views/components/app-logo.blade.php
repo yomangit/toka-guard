1ed8c4be1930499f856fd6167f693039eb0f2691
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-10-31 14:21:54 +0800

    sa

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,6 @@
 <div class="avatar">
     <div class="w-8 rounded">
-        <x-app-logo-icon class=" size-8 fill-current text-white dark:text-black" />
+        <x-app-logo-icon class=" size-9 fill-current text-white dark:text-black" />
     </div>
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
