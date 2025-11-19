956c0dfaab5fa0e6924a31d3a8188226759ab414
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-11-19 09:56:00 +0800

    as

--- resources/views/livewire/administration/custodian/custodian.blade.php
+++ resources/views/livewire/administration/custodian/custodian.blade.php
@@ -7,7 +7,7 @@
             <flux:button size="xs" wire:click='open_modal' icon="add-icon" variant="primary"></flux:button>
         </flux:tooltip>
         <div class='md:flex-row flex-col flex gap-2'>
-            <flux:input size='xs' icon="magnifying-glass" wire:model.live='search_department' placeholder="Search departemen" />
+            <flux:input size='xs' icon="magnifying-glass" wire:model.blur='search_department' placeholder="Search departemen" />
         </div>
     </div>
     <x-manhours.layout>
