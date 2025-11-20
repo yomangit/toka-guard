6959656ac774f8af41a343f724dd59bbb0177837
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-11-20 10:19:19 +0800

    dssdsd

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,6 +1,8 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+<meta name="apple-mobile-web-app-status-bar" content="#01d679">
+<meta name="apple-mobile-web-app-capable" content="yes">
 <title>{{ $title ?? config('app.name') }}</title>
 
 <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
@@ -9,37 +11,44 @@
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 <link rel="manifest" href="/site.webmanifest" />
 
-<!-- Web Application Manifest -->
+
 <link rel="manifest" href="/manifest.json">
-<!-- Chrome for Android theme color -->
-<meta name="theme-color" content="#000000">
 
-<!-- Add to homescreen for Chrome on Android -->
-<meta name="mobile-web-app-capable" content="yes">
-<meta name="application-name" content="PWA">
-<link rel="icon" sizes="512x512" href="/images/icons/icon-512x512.png">
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.bunny.net">
+<link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-<!-- Add to homescreen for Safari on iOS -->
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-status-bar-style" content="black">
-<meta name="apple-mobile-web-app-title" content="PWA">
-<link rel="apple-touch-icon" href="/images/icons/icon-512x512.png">
+<link rel="apple-touch-icon" sizes="16x16" href="/pwa/icons/ios/16.png">
+<link rel="apple-touch-icon" sizes="20x20" href="/pwa/icons/ios/20.png">
+<link rel="apple-touch-icon" sizes="29x29" href="/pwa/icons/ios/29.png">
+<link rel="apple-touch-icon" sizes="32x32" href="/pwa/icons/ios/32.png">
+<link rel="apple-touch-icon" sizes="40x40" href="/pwa/icons/ios/40.png">
+<link rel="apple-touch-icon" sizes="50x50" href="/pwa/icons/ios/50.png">
+<link rel="apple-touch-icon" sizes="57x57" href="/pwa/icons/ios/57.png">
+<link rel="apple-touch-icon" sizes="58x58" href="/pwa/icons/ios/58.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/pwa/icons/ios/60.png">
+<link rel="apple-touch-icon" sizes="64x64" href="/pwa/icons/ios/64.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/pwa/icons/ios/72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/pwa/icons/ios/76.png">
+<link rel="apple-touch-icon" sizes="80x80" href="/pwa/icons/ios/80.png">
+<link rel="apple-touch-icon" sizes="87x87" href="/pwa/icons/ios/87.png">
+<link rel="apple-touch-icon" sizes="100x100" href="/pwa/icons/ios/100.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/pwa/icons/ios/114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/pwa/icons/ios/120.png">
+<link rel="apple-touch-icon" sizes="128x128" href="/pwa/icons/ios/128.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/pwa/icons/ios/144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/pwa/icons/ios/152.png">
+<link rel="apple-touch-icon" sizes="167x167" href="/pwa/icons/ios/167.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/pwa/icons/ios/180.png">
+<link rel="apple-touch-icon" sizes="192x192" href="/pwa/icons/ios/192.png">
+<link rel="apple-touch-icon" sizes="256x256" href="/pwa/icons/ios/256.png">
+<link rel="apple-touch-icon" sizes="512x512" href="/pwa/icons/ios/512.png">
+<link rel="apple-touch-icon" sizes="1024x1024" href="/pwa/icons/ios/1024.png">
 
-<link href="/images/icons/splash-640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-750x1334.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-1242x2208.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-1125x2436.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-828x1792.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-1242x2688.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-1536x2048.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-1668x2224.png" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-1668x2388.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-2048x2732.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-
-<!-- Tile for Win8 -->
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="/images/icons/icon-512x512.png">
-
+<link href="/pwa/icons/ios/1024.png" sizes="1024x1024" rel="apple-touch-startup-image">
+<link href="/pwa/icons/ios/512.png" sizes="512x512" rel="apple-touch-startup-image">
+<link href="/pwa/icons/ios/256.png" sizes="256x256" rel="apple-touch-startup-image">
+<link href="/pwa/icons/ios/192.png" sizes="192x192" rel="apple-touch-startup-image">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 @stack('styles')
