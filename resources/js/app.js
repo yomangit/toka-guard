5ea50460469dcf678ee4a7053f1a8915c4b55ed9
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-11-20 10:47:53 +0800

    sdfsdf

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@ import * as echarts from 'echarts';
 window.echarts = echarts
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function (registration) {
+    navigator.serviceWorker.register('/serviceworker.js', { scope: '/' }).then(function (registration) {
         console.log(`SW registered successfully!`);
     }).catch(function (registrationError) {
         console.log(`SW registration failed`);
